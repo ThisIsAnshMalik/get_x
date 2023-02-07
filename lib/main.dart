14291536043c7be886e088cb05643c9269f65836
Ansh Malik <anshmalik279@gmail.com>
2023-02-07 16:21:44 +0530

    basic home screen and getX dependency added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get_x/view/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: const HomeView(),
     );
   }
 }
