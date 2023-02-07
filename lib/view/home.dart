e8968bb32e9a34075ed1be86f55a1a29319d0ed3
Ansh Malik <anshmalik279@gmail.com>
2023-02-07 16:37:02 +0530

    getX SnackBar widget

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -10,6 +11,17 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Getx State Management"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Get.snackbar("Ansh Malik", "First utils of the Getx",
+              snackPosition: SnackPosition.BOTTOM, backgroundColor: Colors.red);
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
