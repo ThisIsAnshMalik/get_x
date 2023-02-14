b485a7126a8737fe194591407274d296ec8fe99a
Ansh Malik <anshmalik279@gmail.com>
2023-02-14 11:48:05 +0530

    BottomSheet in getX and Changing theme with getX

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -31,6 +31,41 @@ class _HomeViewState extends State<HomeView> {
                       textCancel: "No");
                 },
               ),
+            ),
+            Card(
+              child: ListTile(
+                title: const Text("GetX Bottom Sheet"),
+                subtitle: const Text("GetX Bottom Sheet with Getx"),
+                onTap: () {
+                  Get.bottomSheet(Container(
+                    decoration: const BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            topRight: Radius.circular(30))),
+                    child: Column(
+                      children: [
+                        ListTile(
+                          title: const Text("Light Mode"),
+                          leading: const Icon(Icons.light_mode),
+                          onTap: () {
+                            Get.changeTheme(ThemeData.light());
+                            Get.back();
+                          },
+                        ),
+                        ListTile(
+                          title: const Text("Dark Mode"),
+                          leading: const Icon(Icons.dark_mode),
+                          onTap: () {
+                            Get.changeTheme(ThemeData.dark());
+                            Get.back();
+                          },
+                        )
+                      ],
+                    ),
+                  ));
+                },
+              ),
             )
           ],
         ),
