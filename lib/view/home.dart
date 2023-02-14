6d69bbdb90a66fc1ffba3714b8748b4dc77f54c8
Ansh Malik <anshmalik279@gmail.com>
2023-02-14 11:31:48 +0530

    getX - defaultDialog Utils

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -15,6 +15,26 @@ class _HomeViewState extends State<HomeView> {
       appBar: AppBar(
         title: const Text("Getx State Management"),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Card(
+              child: ListTile(
+                title: const Text("GetX dialog alert"),
+                subtitle: const Text("GetX dialog alert with Getx"),
+                onTap: () {
+                  Get.defaultDialog(
+                      title: "Delete",
+                      middleText: "Second utils of the Getx",
+                      textConfirm: "Yes",
+                      textCancel: "No");
+                },
+              ),
+            )
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Get.snackbar("Ansh Malik", "First utils of the Getx",
